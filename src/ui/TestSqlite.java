9b857aafe665e243c182700f50a3915f34832458
bovink <bovink@sina.com>
2016-09-19 13:35:44 +0800

    添加循环时间

--- src/ui/TestSqlite.java
+++ src/ui/TestSqlite.java
@@ -1,6 +1,11 @@
 package ui;
 
-import sql.BeatmapTable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 
 /**
  * Created by bovink on 2016/9/18.
@@ -11,7 +16,34 @@ public class TestSqlite {
 //        BeatmapTable.createTable();
 //        BeatmapTable.insertBeatmap();
 //        BeatmapTable.queryBeatmap(10000000);
-        BeatmapTable.deleteBeatmap(54899);
+//        BeatmapTable.deleteBeatmap(54899);
+
+        String startTime = "2009-01-01-00:00:00";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss", Locale.CHINA);
+        try {
+            Date date = sdf.parse(startTime);
+            GregorianCalendar calendar = new GregorianCalendar();
+            calendar.setTime(date);
+            while (calendar.get(Calendar.YEAR)  != 2016){
+                calendar.add(Calendar.MONTH, 1);
+                printDate(calendar);
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static void printDate(GregorianCalendar calendar) {
+//        System.out.println("year:" + calendar.get(Calendar.YEAR));
+//        System.out.println("month:" + calendar.get(Calendar.MONTH));
+//        System.out.println("day:" + calendar.get(Calendar.DAY_OF_MONTH));
+        Date date = calendar.getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss", Locale.CHINA);
+        String time = sdf.format(date);
+        System.out.println("time:" + time);
+
     }
 
 
