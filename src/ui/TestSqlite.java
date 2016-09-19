1d660f4fc48ecdc8fdfce13a86246e396906ef4d
bovink <bovink@sina.com>
2016-09-19 17:00:30 +0800

    成功获取所有beatmap数据

--- src/ui/TestSqlite.java
+++ src/ui/TestSqlite.java
@@ -1,12 +1,14 @@
 package ui;
 
+import database.BeatmapTable;
+
 /**
  * Created by bovink on 2016/9/18.
  */
 public class TestSqlite {
 
     public static void main(String[] args) {
-//        BeatmapTable.createTable();
+        BeatmapTable.createTable();
 //        BeatmapTable.insertBeatmap();
 //        BeatmapTable.queryBeatmap(10000000);
 //        BeatmapTable.deleteBeatmap(54899);
